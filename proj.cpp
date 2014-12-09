7d441ec2137cea31fb3a79c7c1052127d1b3b1cc
Lina Inverse <molot@serp>
2014-12-09 11:40:25 -0500

    agregado desordenado del cubo, pero aun gira erroneamente

--- proj.cpp
+++ proj.cpp
@@ -22,6 +22,7 @@
 
 #define  MOV_EJEX	1
 #define  MOV_EJEY	2
+#define  MOV_EJEZ	3
 
 #define	HORARIO		-1
 #define ANTIHORARIO	1
@@ -47,15 +48,14 @@ int seleccionado = -2;
 int direccion = 0;
 int sentido = 0;
 
-float antX, antY;
-int giroAntX, giroAntY;
+float antX, antY, antZ;
+int giroAntX, giroAntY, giroAntZ;
 
 // estructura para inicializar las posiciones 
 // y los colores de las caras de los cubos
 
 typedef struct{
 	float x, y, z;
-//	alfaX, alfaY, alfaZ
 	int alfaX, alfaY, alfaZ;
 	float AntX, AntY, AntZ;
 	int colores[6];
@@ -98,7 +98,7 @@ void escena(void)
 	glRotatef(angZ,0.0,0.0,1.0);
 
 	glLineWidth(1);
-//printf("escena %d\n",modo);
+	
 	for(int i=0; i<numCubos; i++){
 	glLoadName(i);
 	glPushMatrix();
@@ -312,14 +312,26 @@ void coordenadaCubo (int * a, int * b, int * c) {
 }
 
 void giraCubo (int cuboAux, int ang){
+	float auxH, auxV;
+	
 	switch (direccion) {
 		case MOV_EJEX:
-			modelo[cuboAux].z = (modelo[cuboAux].z - 2*ladoCubo/2.0)*cos(ang*GL_PI/180.0) - (modelo[cuboAux].y - 2*ladoCubo/2.0)*sin(ang*GL_PI/180.0) + 2*ladoCubo/2.0;
-			modelo[cuboAux].y = (modelo[cuboAux].z - 2*ladoCubo/2.0)*sin(ang*GL_PI/180.0) + (modelo[cuboAux].y - 2*ladoCubo/2.0)*cos(ang*GL_PI/180.0) + 2*ladoCubo/2.0;
+			auxH=modelo[cuboAux].y;
+			auxV=modelo[cuboAux].z;
+			modelo[cuboAux].y = (auxH - 2*ladoCubo/2.0)*cos(ang*GL_PI/180.0) - (auxV - 2*ladoCubo/2.0)*sin(ang*GL_PI/180.0) + 2*ladoCubo/2.0;
+			modelo[cuboAux].z = (auxH - 2*ladoCubo/2.0)*sin(ang*GL_PI/180.0) + (auxV - 2*ladoCubo/2.0)*cos(ang*GL_PI/180.0) + 2*ladoCubo/2.0;
 		break;
 		case MOV_EJEY:
-			modelo[cuboAux].x = (modelo[cuboAux].x - 2*ladoCubo/2.0)*cos(ang*GL_PI/180.0) - (modelo[cuboAux].z - 2*ladoCubo/2.0)*sin(ang*GL_PI/180.0) + 2*ladoCubo/2.0;
-			modelo[cuboAux].z = (modelo[cuboAux].x - 2*ladoCubo/2.0)*sin(ang*GL_PI/180.0) + (modelo[cuboAux].z - 2*ladoCubo/2.0)*cos(ang*GL_PI/180.0) + 2*ladoCubo/2.0;
+			auxH=modelo[cuboAux].z;
+			auxV=modelo[cuboAux].x;
+			modelo[cuboAux].z = (auxH - 2*ladoCubo/2.0)*cos(ang*GL_PI/180.0) - (auxV - 2*ladoCubo/2.0)*sin(ang*GL_PI/180.0) + 2*ladoCubo/2.0;
+			modelo[cuboAux].x = (auxH - 2*ladoCubo/2.0)*sin(ang*GL_PI/180.0) + (auxV - 2*ladoCubo/2.0)*cos(ang*GL_PI/180.0) + 2*ladoCubo/2.0;
+		break;
+		case MOV_EJEZ:
+			auxH=modelo[cuboAux].x;
+			auxV=modelo[cuboAux].y;
+			modelo[cuboAux].x = (auxH - 2*ladoCubo/2.0)*cos(ang*GL_PI/180.0) - (auxV - 2*ladoCubo/2.0)*sin(ang*GL_PI/180.0) + 2*ladoCubo/2.0;
+			modelo[cuboAux].y = (auxH - 2*ladoCubo/2.0)*sin(ang*GL_PI/180.0) + (auxV - 2*ladoCubo/2.0)*cos(ang*GL_PI/180.0) + 2*ladoCubo/2.0;
 		break;
 	}
 }
@@ -347,7 +359,44 @@ void animacionCubo(){
 					modelo[auxCubo].alfaY += sentido*3;
 				}
 		break;
+		case MOV_EJEZ:
+			for (i = 0; i < 2; i++)
+				for (j = 0; j < 2; j++){
+					auxCubo = rubikLogico[i][j][z];
+					giraCubo (auxCubo, sentido*3);
+					modelo[auxCubo].alfaZ += sentido*3;
+				}
+		break;
 	}
+}
+
+int giros(){
+	int giros, dif;
+	
+	switch (direccion) {
+		case MOV_EJEX:
+			while (modelo[seleccionado].alfaX < 0) modelo[seleccionado].alfaX += 360;
+			dif = modelo[seleccionado].alfaX - giroAntX;
+			if (dif < 0) dif += 360;
+			giros = (dif + 45)/90;
+		break;
+		
+		case MOV_EJEY:
+			while (modelo[seleccionado].alfaY < 0) modelo[seleccionado].alfaY += 360;
+			dif = modelo[seleccionado].alfaY - giroAntY;
+			if (dif < 0) dif += 360;
+			giros = (dif + 45)/90;
+		break;
+			
+		case MOV_EJEZ:
+			while (modelo[seleccionado].alfaY < 0) modelo[seleccionado].alfaZ += 360;
+			dif = modelo[seleccionado].alfaZ - giroAntZ;
+			if (dif < 0) dif += 360;
+			giros = (dif + 45)/90;
+		break;
+	}
+	
+	return giros;
 }
 
 void soltarRubik(){
@@ -359,10 +408,7 @@ void soltarRubik(){
 
 	switch (direccion) {
 		case MOV_EJEX:
-			if (modelo[seleccionado].alfaX < 0) modelo[seleccionado].alfaX += 360;
-			dif = modelo[seleccionado].alfaX - giroAntX;
-			if (dif < 0) dif += 360;
-			numRot = ((dif + 45)/90)%4;
+			numRot = giros();
 			coordenadaCubo(&x, &y, &z);
 			for (i = 0; i < 2; i++)	for (j = 0; j < 2; j++){
 				cuboAux = rubikLogico[x][i][j];
@@ -385,10 +431,7 @@ void soltarRubik(){
 		break;
 			
 		case MOV_EJEY:
-			if (modelo[seleccionado].alfaY < 0) modelo[seleccionado].alfaY += 360;
-			dif = modelo[seleccionado].alfaY - giroAntY;
-			if (dif < 0) dif += 360;
-			numRot = ((dif + 45)/90)%4;
+			numRot = giros();
 			coordenadaCubo(&x, &y, &z);
 			for (i = 0; i < 2; i++)	for (j = 0; j < 2; j++){
 				cuboAux = rubikLogico[i][y][j];
@@ -410,8 +453,59 @@ void soltarRubik(){
 			rubikLogico[1][y][0]=posAux[(numRot+3)%4];
 		break;
 			
+		case MOV_EJEZ:
+			numRot = giros();
+			coordenadaCubo(&x, &y, &z);
+			for (i = 0; i < 2; i++)	for (j = 0; j < 2; j++){
+				cuboAux = rubikLogico[i][j][z];
+				modelo[cuboAux].x = modelo[cuboAux].AntX;
+				modelo[cuboAux].y = modelo[cuboAux].AntY;
+				giraCubo(cuboAux, (numRot*90));
+				modelo[cuboAux].AntX = modelo[cuboAux].x;
+				modelo[cuboAux].AntY = modelo[cuboAux].y;
+				modelo[cuboAux].alfaZ = (90*numRot+giroAntZ) % 360;
+			}
+			posAux[0]=rubikLogico[0][0][z];
+			posAux[1]=rubikLogico[1][0][z];
+			posAux[2]=rubikLogico[1][1][z];
+			posAux[3]=rubikLogico[0][1][z];
+			
+			rubikLogico[0][0][z]=posAux[(numRot)%4];
+			rubikLogico[1][0][z]=posAux[(numRot+1)%4];
+			rubikLogico[1][1][z]=posAux[(numRot+2)%4];
+			rubikLogico[0][1][z]=posAux[(numRot+3)%4];
+		break;
 	}
-//	escena();
+}
+
+void desordenarRubik(){
+	float  aleat = (float)rand()/RAND_MAX;//0y1
+	int numero = int(aleat*5);
+	int giros;
+	
+	for(int i=0; i<numero; i++){
+		aleat = (float)rand()/RAND_MAX;
+		seleccionado = int(aleat*numCubos);
+		printf("%d\n",seleccionado);
+		aleat = (float)rand()/RAND_MAX;
+		giros = int( aleat*4);
+		printf("%d\n",giros);
+		aleat = (float)rand()/RAND_MAX;
+		direccion = int( aleat*3)+1;
+		printf("%d\n",direccion);
+		aleat = (float)rand()/RAND_MAX;
+		sentido = 2*( int( aleat*2))-1;
+		printf("%d\n",sentido);
+		for(int ang=0; ang<(giros*90);ang=ang+3){
+			animacionCubo();
+			escena();
+		}
+		soltarRubik();
+		escena();
+	}
+	seleccionado=-1;
+	direccion = 0;
+	sentido = 0;
 }
 
 void transformar (){
@@ -422,19 +516,21 @@ void transformar (){
 
 void tecladoS(int tecla, int x, int y)
 {
-	if(tecla == GLUT_KEY_UP && modo==MODO_ROTACION){
-		enfocar+=0.02;
-//printf("%.2f\n",angX);
-		reshape(ventanaX, ventanaY);
-		escena();
-	}
-
-	if(tecla == GLUT_KEY_DOWN && modo==MODO_ROTACION){
-		if(enfocar > 0.02)	enfocar-=0.02;
-		else enfocar = 0.01;
-//printf("%.2f\n",angX);
-		reshape(ventanaX, ventanaY);
-		escena();
+	if(modo==MODO_ROTACION){
+	
+		if(tecla == GLUT_KEY_UP ){
+			enfocar+=0.02;
+	//printf("%.2f\n",angX);
+			reshape(ventanaX, ventanaY);
+			escena();
+		}else if(tecla == GLUT_KEY_DOWN ){
+			if(enfocar > 0.02)	enfocar-=0.02;
+			else enfocar = 0.01;
+	//printf("%.2f\n",angX);
+			reshape(ventanaX, ventanaY);
+			escena();
+		}
+	
 	}
 }
 
@@ -466,7 +562,8 @@ void mouse(int boton,int estado,int x,int y )
 							antX = x;
 							antY = y;
 							giroAntX = modelo[seleccionado].alfaX;
-							giroAntY = modelo[seleccionado].alfaY;}
+							giroAntY = modelo[seleccionado].alfaY;
+							giroAntZ = modelo[seleccionado].alfaZ;}
 					break;
 				}
 			break;
@@ -520,7 +617,6 @@ void rotarRaton(int x, int y){
 					}
 					antX = x;
 					antY = y;
-			//		escena();
 				break;
 				case false:
 //					direccion = -1;
@@ -558,7 +654,6 @@ void rotarRaton(int x, int y){
 					}
 //					antX = x;
 //					antY = y;
-			//		escena()
 				break;
 				case false:
 //					direccion = -1;
@@ -581,8 +676,18 @@ void reshape(int w, int h)
 	glOrtho (-size, size, -size / redimension, size/ redimension, 7.0f*enfocar, -7.0f*enfocar);
 	else
 	glOrtho (-size* redimension, size* redimension, -size, size, 7.0f*enfocar, -7.0f*enfocar);
-
 //	glMatrixMode(GL_MODELVIEW);
+}
+
+void submen(int value){
+	switch(value){
+		case 1 :
+			modo=MODO_JUEGO;
+			break;
+		case 2 :
+			desordenarRubik();
+			break;
+	}
 }
 
 void principal(int value){
@@ -590,9 +695,9 @@ void principal(int value){
 		case 1 :
 			modo=MODO_ROTACION;
 			break;
-		case 2 :
+/*		case 2 :
 			modo=MODO_JUEGO;
-			break;
+			break;*/
 		case 3 :
 			modo=MODO_STANDBY;
 			break;
@@ -617,9 +722,14 @@ int main(int argc, char** argv){
 	glutSpecialFunc(tecladoS);
 	glutKeyboardFunc(teclado);
 	
+	
+	int submenu = glutCreateMenu(submen);
+	glutAddMenuEntry("Empezar",1);
+	glutAddMenuEntry("Desordenar Rubik",2);
+	
 	glutCreateMenu(principal);
 	glutAddMenuEntry("Modo Rotacion",1);
-	glutAddMenuEntry("Modo Juego",2);
+	glutAddSubMenu("Modo Juego",submenu);
 	glutAddMenuEntry("Modo Stand By",3);
 	
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
